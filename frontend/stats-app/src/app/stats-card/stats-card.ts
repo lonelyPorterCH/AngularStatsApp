25b1015939199e011337c0d2161bf697e6eb801f
lonelyPorterCH <fabio.stuber@gmail.com>
2026-04-24 11:46:54 +0200

    Route from stats-card to stat-details

--- frontend/stats-app/src/app/stats-card/stats-card.ts
+++ frontend/stats-app/src/app/stats-card/stats-card.ts
@@ -1,11 +1,13 @@
 import {Component, Input} from '@angular/core';
 import {MatButton} from "@angular/material/button";
 import {Stat} from '../models/stat.model';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-stats-card',
   imports: [
-    MatButton
+    MatButton,
+    RouterLink
   ],
   templateUrl: './stats-card.html',
   styleUrl: './stats-card.css',
