0e45981ca7729588a8ce7718213f5338b088f40e
lonelyPorterCH <fabio.stuber@gmail.com>
2026-04-28 11:28:25 +0200

    Navigate to main page when clicking on toolbar text

--- frontend/stats-app/src/app/app.ts
+++ frontend/stats-app/src/app/app.ts
@@ -1,11 +1,11 @@
 import {Component} from '@angular/core';
 import {MatToolbar} from '@angular/material/toolbar';
 import {MatIcon} from '@angular/material/icon';
-import {RouterOutlet} from '@angular/router';
+import {RouterLink, RouterOutlet} from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [MatToolbar, MatIcon, RouterOutlet],
+  imports: [MatToolbar, MatIcon, RouterOutlet, RouterLink],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
